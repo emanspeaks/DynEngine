f626be0a609b8d29729d36e0f6d6424b2f25e085
Randy Eckman <randy.eckman@nasa.gov>
2023-12-18 10:12:12 -0600

    now that my updates to utf8 were merged upstream, revert to upstream utf8 pkg

--- cmake/utf8.cmake
+++ cmake/utf8.cmake
@@ -2,7 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
   utf8
-  GIT_REPOSITORY "https://github.com/emanspeaks/utf8.git"
-  GIT_TAG cmake-fixes
+  GIT_REPOSITORY "https://github.com/neacsum/utf8.git"
+  # GIT_REPOSITORY "https://github.com/emanspeaks/utf8.git"
+  GIT_TAG main
 )
 FetchContent_MakeAvailable(utf8)
